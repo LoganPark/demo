857a22cf88128412761c658eef72c0e563fbb5d1
Logan Park <logan@logan-park.org>
2015-04-30 16:57:55 -0500

    Added a quick and dirty minimal windowed overhead view.

---
+++ Assets/Minimapper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimapper : MonoBehaviour {
+
+	public Texture MinimapRenderTexture;
+	
+	void OnGUI () {
+	
+		GUILayout.BeginArea(new Rect(Screen.width * 0.8f, 0, Screen.width * 0.2f, Screen.width * 0.2f));
+			GUILayout.Box( MinimapRenderTexture );
+		GUILayout.EndArea();
+	}
+}
